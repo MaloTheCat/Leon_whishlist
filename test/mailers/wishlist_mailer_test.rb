09ec8aae79c1358b62dcd9f49c8d40f214053409
MaloTheCat <contact@jonathan-coz.com>
2025-11-06 20:48:28 +0100

    Fix lint for pipeline

--- test/mailers/wishlist_mailer_test.rb
+++ test/mailers/wishlist_mailer_test.rb
@@ -2,9 +2,11 @@ require "test_helper"
 
 class WishlistMailerTest < ActionMailer::TestCase
   test "share_wishlist" do
-    mail = WishlistMailer.share_wishlist
+    wishlist = Wishlist.first
+    recipient_email = "to@example.org"
+    mail = WishlistMailer.share_wishlist(wishlist, recipient_email)
     assert_equal "Share wishlist", mail.subject
-    assert_equal ["to@example.org"], mail.to
+    assert_equal [recipient_email], mail.to
     assert_equal ["from@example.com"], mail.from
     assert_match "Hi", mail.body.encoded
   end
