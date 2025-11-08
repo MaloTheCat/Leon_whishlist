45869c485ab1249ab24772b608cb36090f545001
MaloTheCat <contact@jonathan-coz.com>
2025-11-09 00:00:41 +0100

    Fix test on pipeline

--- test/mailers/wishlist_mailer_test.rb
+++ test/mailers/wishlist_mailer_test.rb
@@ -1,13 +1,12 @@
 require "test_helper"
 
 class WishlistMailerTest < ActionMailer::TestCase
-  # test "share_wishlist" do
-  #   wishlist = Wishlist.first
-  #   recipient_email = "to@example.org"
-  #   mail = WishlistMailer.share_wishlist(wishlist, recipient_email)
-  #   assert_equal "Share wishlist", mail.subject
-  #   assert_equal [recipient_email], mail.to
-  #   assert_equal ["from@example.com"], mail.from
-  #   assert_match "Hi", mail.body.encoded
-  # end
+  test "share_wishlist" do
+    wishlist = wishlists(:one)
+    recipient_email = "to@example.org"
+    mail = WishlistMailer.share_wishlist(wishlist, recipient_email)
+    assert_equal "John Doe partage sa liste de cadeaux avec vous !", mail.subject
+    assert_equal [recipient_email], mail.to
+    assert_match "John Doe", mail.body.encoded
+  end
 end
